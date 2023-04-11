07ca588f5861667a39b7ac939bc54da9d43d1e55
cecloudx <admin@cec.cc>
2023-04-11 20:18:57 +0000

    modify

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -11,12 +11,13 @@ export default defineConfig({
 
     outlineTitle: '本页目录',
     // outline:2,
-    
+
+
     algolia: {
         appId: 'N26XKP71HQ', // 需要替换
         apiKey: 'b68973e86f1cea6fe107eecb587b3d40', // 需要替换
         indexName: 'cc-index', // 需要替换
-        placeholder: '请输入关键词',
+        // placeholder: '请输入关键词',
         // buttonText: '搜索'
     },
     // https://vitepress.dev/reference/default-theme-config
